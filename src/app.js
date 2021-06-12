a0c03ff38c07e692680287ee4cdeb2f4a8375130
jian.chen <ginkgo985@gmail.com>
2021-06-12 11:13:41 +0800

    use catchAsync && CORS

--- src/app.js
+++ src/app.js
@@ -11,8 +11,14 @@ const {getAbout} = require("./controllers/abouts-controller");
 const {getUsers} = require("./controllers/users-controller");
 const {logger} = require("./middlewares/mylogger_middleware");
 const {handleErrors} = require("./middlewares/handle_errors");
+const cors = require('cors')
 
 
+const catchAsync = fn => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next);
+    };
+};
 
 
 (async function() {
@@ -24,13 +30,15 @@ const {handleErrors} = require("./middlewares/handle_errors");
     app.set('view engine', 'ejs');
 
     app.use(logger)
+    app.use(cors())
 
-    app.get('/users', getUsers)
+
+    app.get('/users', catchAsync(getUsers))
     app.get('/courses', getCourses)
     app.get('/courses/:id', getCourse)
     app.post('/courses/:id/delete', deleteCourse)
     app.post('/add_course', addCourseToUser)
-    app.get('/users/:id', getUser)
+    app.get('/users/:id', catchAsync(getUser))
     app.post('/users', createUser)
     app.get('/about',getAbout)
 
--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,32 +1,17 @@
-// const {User} = require("../../deprecated/User");
-const {mClient} = require("../lib/mysql_client");
 var _ = require('lodash')
-
-const {Course} = require("../../deprecated/Course");
-
 const db = require("../models")
 const {UserNotFound} = require("../middlewares/handle_errors");
 
 
-
-
-
 const getUser = async function (req, res, next) {
-
-    try {
-        const {id} = req.params;
-        console.log("> getUser id: %s", id)
-        const user = await db.User.findByPk(id)
-
-        if (!user) {
-            throw new UserNotFound(`No User Found (id: ${id})`)
-        }
-
-        const courses = await user.getCourses();
-        res.json({user: user.toJSON(), courses: courses.map(c =>c.toJSON())})
-    }catch (err) {
-        next(err)
+    const {id} = req.params;
+    console.log("> getUser id: %s", id)
+    const user = await db.User.findByPk(id)
+    if (!user) {
+        throw new UserNotFound(`No User Found (id: ${id})`)
     }
+    const courses = await user.getCourses();
+    res.json({user: user.toJSON(), courses: courses.map(c =>c.toJSON())})
 }
 
 
